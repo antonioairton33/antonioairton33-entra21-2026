d25f0576be6f71089790782ae1e4c492fd15f3f8
Antonio Airton <antonioairton18a@gmail.com>
2026-04-05 01:05:58 -0300

    Exercicio 7,8,9,10

--- Aula03/src/ListaExercicio2.java
+++ Aula03/src/ListaExercicio2.java
@@ -11,10 +11,10 @@ public class ListaExercicio2 {
 		//exercicio5();
 		
 		//nivel2exercicio6();
-		nivel2exercicio7();
+		//nivel2exercicio7();
 		//nivel2exercicio8();
 		//nivel2exercicio9();
-		//nivel2exercicio10();
+		nivel2exercicio10();
 		
 		
 		
@@ -109,14 +109,14 @@ public class ListaExercicio2 {
 	}
 	
 	static void nivel2exercicio6() {
-		int numero1, numero2, numero3, resultado;
+		int numero1, numero2, numero3;
 		Scanner entradaUser = new Scanner(System.in);
 		
-		System.out.println("Informe o numero1:");
+		System.out.println("Informe o primeiro numero: ");
 		numero1 = entradaUser.nextInt();
-		System.out.println("Informe o numero2:");
+		System.out.println("Informe o segundo numero: ");
 		numero2 = entradaUser.nextInt();
-		System.out.println("Informe o numero3:");
+		System.out.println("Informe o terceiro numero: ");
 		numero3 = entradaUser.nextInt();
 		
 		if(numero1 > numero2) {
@@ -138,23 +138,99 @@ public class ListaExercicio2 {
 	}
 	
 	static void nivel2exercicio7() {
-		int idade;
+		int nota;
 		
 		Scanner entradaUser = new Scanner(System.in);
 		
-		System.out.println("Informe sua idade:");
-		idade = entradaUser.nextInt();
+		System.out.println("Informe uma nota de 0 a 10: ");
+		nota = entradaUser.nextInt();
 		
-		if (idade >= 16) {
-			System.out.println("Você pode votar.");
+		if (nota >= 0 && nota <= 4 ) {
+			System.out.println("Insuficiente");
+		} else if (nota >= 5 && nota <=6){
+			System.out.println("Regular.");
+		} else if (nota >= 7 && nota <= 8) {
+			System.out.println("Bom.");
+		} else if (nota >= 9 && nota <= 10) {
+			System.out.println("Execelente");
 		} else {
-			System.out.println("Você não pode votar.");
+			System.out.println("Nota inválida!!!");
+		}
+		
+	}
+	
+	static void nivel2exercicio8 () {
+		int lado1, lado2, lado3;
+		
+		Scanner entradaUser = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro lado triangulo: ");
+		lado1 = entradaUser.nextInt();
+		System.out.println("Informe o segundo lado triangulo: ");
+		lado2 = entradaUser.nextInt();
+		System.out.println("Informe o terceiro lado triangulo: ");
+		lado3 = entradaUser.nextInt();
+		
+		if (lado1 + lado2 > lado3 &&  lado1 + lado3 > lado2 && lado2 + lado3 > lado1 ) {
+			System.out.println("Foma um triângulo.");
+		} else {
+			System.out.println("Não corresponde a um triângulo.");
 		}
 		
 		
 		
 	}
 	
+	static void nivel2exercicio9 () {
+		int lado1, lado2, lado3;
+		
+		Scanner entradaUser = new Scanner(System.in);
+		
+		System.out.println("Informe o primeiro lado triangulo: ");
+		lado1 = entradaUser.nextInt();
+		System.out.println("Informe o segundo lado triangulo: ");
+		lado2 = entradaUser.nextInt();
+		System.out.println("Informe o terceiro lado triangulo: ");
+		lado3 = entradaUser.nextInt();
+		
+		if (lado1 == lado2 && lado2 == lado3) {
+			System.out.println("Triângulo Equilátero.");
+		} else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+			System.out.println("Triangulo Isósceles.");
+		} else if (lado1 != lado2  && lado1 != lado3 && lado2 != lado3) {
+			System.out.println("Triângulo Escaleno");
+		} else {
+			System.out.println("Não corresponde a nenhum triângulo apresentado.");
+		}
+		
+		
+		
+	}
+	
+	static void nivel2exercicio10 () {
+		/*Usuário: Batata
+		  Senha: 54321	
+		  */
+		
+		String usuario;
+		int senha;
+		
+		Scanner entradaUser = new Scanner(System.in);
+		
+		System.out.println("Informe seu Usuário ");
+		usuario = entradaUser.nextLine();
+		System.out.println("Informe sua Senha: ");
+		senha = entradaUser.nextInt();
+	
+		
+		if (usuario.equals("Batata")  && senha == 54321) {
+			System.out.println("Acesso permitido.");
+		} else {
+			System.out.println("Acesso Negado.");
+		} 
+		
+		
+	}
 	
 	
 	
