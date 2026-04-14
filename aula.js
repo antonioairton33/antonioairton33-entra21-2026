3fedd17cf7c33846ea000c6dda7b269c67493b4b
Antonio Sobrinho <antonio63466786@alunos.sc.senac.br>
2026-04-14 16:09:54 -0300

    subindo branch js

---
+++ aula.js
@@ -0,0 +1,20 @@
+function variaveis(){
+
+    const nome = 'ana'
+
+    var idade = 10
+
+    let altura = 1.69
+
+    console.log(nome)
+    console.log(idade)
+    console.log(altura)
+
+}
+
+function ola(){
+    console.log("ola")
+}
+
+ola()
+variaveis()
